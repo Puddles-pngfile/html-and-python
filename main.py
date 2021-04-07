957c187863510e8c701a8ee09c92468071777f75
Puddles-pngfile <thomas.munday23@gmail.com>
2021-04-07 18:36:15 +0100

    reset ):

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from flask import Flask, redirect, url_for, render_template
 import random
+
+
+
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def home():
-    name = "Verge"
-    return render_template("index.html")
+    name = "i lick toes"
+    return render_template("index.html", content=name)
 
-@app.route("/rng")
-def numgen():
-    rand = random.randint(0,1000)
-    return render_template("randnum.html")
-
- 
-
-if __main__ == "__name__":
-    app.run()
+@app.route("/aboutme")
+def aboutme():
+    rand = random.randint(0,100)
+    return render_template("aboutme.html", rand = rand )
 
 
 
 
 
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
+
+
+  
